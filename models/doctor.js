c8cc2a8bdf81805fbd8073721cf636f4fb9f8036
Abhay 076 <yadavabhaycs@gmail.com>
2022-12-23 19:35:59 +0530

    hospitalApi

---
+++ models/doctor.js
@@ -0,0 +1,27 @@
+//require mongoose
+const mongoose = require("mongoose");
+//doctors Schema
+const docSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// exports user
+const Doctor = mongoose.model("Doctor", docSchema);
+module.exports = Doctor;
